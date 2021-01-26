e857503fb45b757b074671a622c0d87523e40db5
Piero Giusti <pierophp@gmail.com>
2021-01-25 22:35:43 -0300

    Fix Dev

--- lib/screens/index_screen.dart
+++ lib/screens/index_screen.dart
@@ -12,6 +12,9 @@ import 'package:LucaPlay/widgets/modals/upsert_playlist_modal.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+// import 'package:filesystem_picker/filesystem_picker.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:permission_handler/permission_handler.dart';
 
 class IndexScreen extends StatefulWidget {
   Box<Playlist> playlistBox;
@@ -28,6 +31,8 @@ class IndexScreen extends StatefulWidget {
 class _IndexScreenState extends State<IndexScreen> {
   bool _buttonImportLoading = false;
 
+  // FileTileSelectMode filePickerSelectMode = FileTileSelectMode.checkButton;
+
   Future<void> handleImportFile() async {
     setState(() {
       _buttonImportLoading = true;
@@ -39,8 +44,22 @@ class _IndexScreenState extends State<IndexScreen> {
       //   allowedExtensions: ['json'],
       // );
 
-      // if (result?.files.single.path != null) {
-      //   File file = File(result!.files.single.path!);
+      // final rootPath = await getTemporaryDirectory();
+
+      // String path = await FilesystemPicker.open(
+      //   title: 'Open file',
+      //   context: context,
+      //   rootDirectory: rootPath,
+      //   fsType: FilesystemType.file,
+      //   folderIconColor: Colors.teal,
+      //   allowedExtensions: ['.json'],
+      //   fileTileSelectMode: filePickerSelectMode,
+      //   requestPermission: () async =>
+      //       await Permission.storage.request().isGranted,
+      // );
+
+      // if (path != null) {
+      //   File file = File(path);
 
       //   final importedObject = jsonDecode(await file.readAsString());
 
